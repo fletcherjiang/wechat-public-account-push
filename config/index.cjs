8f9904d1461c1143fe661d6e4c9248807d7ad0df
JIANG Yiyang <a@imjyy.com>
2023-03-15 20:45:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: 'wx54226c530e7f3294',
+  APP_ID: 'wx5d93b340d864d5d2',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: 'cf12fd5c1bbae5eb25a3592fbb8b478c',
+  APP_SECRET: '4160eeb960f8f564e857508f629ccd9f',
 
   PROVINCE: '重庆',
   CITY: '重庆',
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小龚',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ouHlV6GeQM4EHAXHz9f6KpY3xdrs',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'V8JdwpJJSofTycEtttgSLDf_YkbGvT25VIPho0Xie68',
+      useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-16',
       festivals: [
@@ -53,13 +53,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '32S4-IeX-Q3_RHwjJQYb3D4nXek5fKdpHEk-pzWTzjg',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ouHlV6GeQM4EHAXHz9f6KpY3xdrs',
+      id: 'oTxqa6FOjkek6sVrkiPUhIZffQe4',
     }
   ],
 
