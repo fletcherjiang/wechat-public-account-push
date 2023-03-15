1dabcaf990ac85d89d20d7433dda37c662919c12
JIANG Yiyang <a@imjyy.com>
2023-03-15 19:08:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ouHlV6GeQM4EHAXHz9f6KpY3xdrs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'eHAjTn741SF2LGfAuOCiH5-ImbGUfKUyUQxwgnu5lEY',
+      useTemplateId: 'V8JdwpJJSofTycEtttgSLDf_YkbGvT25VIPho0Xie68',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-16',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '32S4-IeX-Q3_RHwjJQYb3D4nXek5fKdpHEk-pzWTzjg',
 
   CALLBACK_USERS: [
     {
